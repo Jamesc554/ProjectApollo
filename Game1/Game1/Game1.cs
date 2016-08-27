ad78531cbbbbf34bb9027f8af1a5bde2d06a2a4d
James Carter <jamesc554544@gmail.com>
2016-08-27 17:13:47 +0100

    Test

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -11,7 +11,7 @@ namespace Game1
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D texture;
+        Texture2D texture; //
 
         public Game1()
         {
